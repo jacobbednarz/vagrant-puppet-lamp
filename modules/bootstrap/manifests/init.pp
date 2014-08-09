b2c6f9b9b00bf7ba45bfae36d761cdd0a4110b93
Jacob Bednarz <jacob.bednarz@gmail.com>
2014-08-09 16:51:01 +1000

    Add in --fix-missing for initial apt-update

--- modules/bootstrap/manifests/init.pp
+++ modules/bootstrap/manifests/init.pp
@@ -6,6 +6,6 @@ class bootstrap {
 
   # Ensure local apt cache index is up to date before beginning.
   exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
+    command => 'apt-get update --fix-missing'
   }
 }
